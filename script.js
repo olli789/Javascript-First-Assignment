4de67aa9e588b6541f400230a56aabbcd9fa19e0
Oliver Petrovic <oliverpetrovic97@hotmail.com>
2020-12-07 12:20:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Data Og */
+const Team1Score = 96 + 108 + 89 / 3
+
+// Alliance */
+const Team2Score = 88 + 91 + 110 / 3
+
+// Data Og Result */
+console.log(Team1Score);
+
+// Alliance Result */
+console.log(Team2Score);
+
+if (Team1Score > Team2Score)
+
+{
+    console.log(`Data Og Won!`)
+}
+
+else  {
+    console.log (`Alliance Won!`)
+}
